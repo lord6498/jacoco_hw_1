578f04dc9d73d1416b890765c7e10fd1f5b2c289
Nick <>
2021-02-05 12:31:40 +0300

    new parametr test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -1,20 +1,28 @@
 package ru.netology.statistic;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StatisticsServiceTest {
+  StatisticsService service = new StatisticsService();
 
-  @Test
-  void findMax() {
-    StatisticsService service = new StatisticsService();
-
-    long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-    long expected = 12;
-
-    long actual = service.findMax(incomesInBillions);
-
-    assertEquals(expected, actual);
+  @ParameterizedTest
+  @MethodSource("maxArguments")
+  void max(long[]incomesInBillions, int expected) {
+    assertEquals(expected, service.findMax(incomesInBillions));
   }
+  public static Stream<Arguments> maxArguments() {
+    long[] first = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+    long[] second = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 13};
+
+    return Stream.of(Arguments.of(first,12),
+                    Arguments.of(second,13));
+  }
+
 }
